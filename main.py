1db7b06482bb16107eb6e34ab5c483263caab256
Jon-Alc <jon.f.alcantara@gmail.com>
2025-02-14 16:30:31 -0800

    Separates file reading into another function and adds function to count characters

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-def count_words():
+from collections import defaultdict
+
+def read_text() -> str:
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(len(file_contents.split()))
+    
+    return file_contents
+
+
+
+def count_words(file_contents: str):
+    print(len(file_contents.split()))
+
+
+
+def count_characters(file_contents: str):
+    
+    lowercase_contents = file_contents.lower()
+    char_count = defaultdict(int)
+
+    for chr in lowercase_contents:
+        char_count[chr] += 1
+    
+    print(char_count)
+
+
 
 if __name__ == "__main__":
-    count_words()
+    file_contents = read_text()
+    print(type(file_contents))
+    count_words(file_contents)
+    count_characters(file_contents)
