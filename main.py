832307851540041e7cb67823ddec78fb2c607b09
Jon-Alc <jon.f.alcantara@gmail.com>
2025-02-14 16:25:12 -0800

    Creates function to count words in text

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(len(file_contents.split()))
+
+if __name__ == "__main__":
+    count_words()
