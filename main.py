06a90411f240a65bce27d9daa25b6cb67137b94a
Jon-Alc <jon.f.alcantara@gmail.com>
2025-02-14 16:38:26 -0800

    Prints report of word count and alphabet character occurrences found in a text

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 from collections import defaultdict
 
-def read_text() -> str:
 
-    with open("books/frankenstein.txt") as f:
+
+def read_text(path: str) -> str:
+
+    with open(path) as f:
         file_contents = f.read()
     
     return file_contents
 
 
 
-def count_words(file_contents: str):
-    print(len(file_contents.split()))
+def count_words(file_contents: str) -> int:
+    return len(file_contents.split())
 
 
 
-def count_characters(file_contents: str):
+def count_characters(file_contents: str) -> defaultdict[str]:
     
     lowercase_contents = file_contents.lower()
     char_count = defaultdict(int)
 
     for chr in lowercase_contents:
-        char_count[chr] += 1
+        if (chr.isalpha()):
+            char_count[chr] += 1
     
-    print(char_count)
+    return char_count
 
 
 
 if __name__ == "__main__":
-    file_contents = read_text()
-    print(type(file_contents))
-    count_words(file_contents)
-    count_characters(file_contents)
+
+    path = "books/frankenstein.txt"
+    
+    file_contents = read_text(path)
+
+    char_count = count_characters(file_contents)
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{count_words(file_contents)} words were found in the document\n")
+
+    for key in char_count.keys():
+        print(f"The '{key}' character was found {char_count[key]} times")
+
+    print("--- End report ---")
+
